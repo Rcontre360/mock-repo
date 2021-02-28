9a85f167cba9e661659ee6e48196614a86335e8a
Rcontre360 <rcontreraspimentel@gmail.com>
2021-02-28 07:41:44 +0000

    add code in files types: ,.json,.html,.txt,.js,.svg,.scss,.bak,.css remove code in files types:

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 import git
 from git_contributions_importer import *
 
-repo = git.Repo("/home/rafael/Desktop/work/bizorRepo/bizor-next")
+repo = git.Repo("/home/rafael/Desktop/work/ongoing/ongoing")
 mock_repo = git.Repo("/home/rafael/Desktop/sandbox/gitRepos/contributionImporter")
 
 importer = Importer([repo], mock_repo)
-importer.set_author(["rcontreraspimentel@gmail.com","sphere@bizor.io"])
+importer.set_author(["rcontreraspimentel@gmail.com"])
 
 importer.import_repository()

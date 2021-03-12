3abca9c35572cb3979e456b58917c7eff2eefa25
Rcontre360 <rcontreraspimentel@gmail.com>
2021-03-11 23:43:44 -0400

    fixed errors when setting up folders

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 import git
 from git_contributions_importer import *
 
-repo = git.Repo("https://Rcontre360@bitbucket.org/bizor/bizor-next.git")
-mock_repo = git.Repo("https://github.com/Rcontre360/mock-repo.git")
+repo = git.Repo("/home/rafael/Desktop/work/bizorRepo/bizor-next")
+mock_repo = git.Repo("/home/rafael/Desktop/sandbox/gitRepos/contributionImporter")
 
 importer = Importer([repo], mock_repo)
 importer.set_author(["rcontreraspimentel@gmail.com","sphere@bizor.io"])

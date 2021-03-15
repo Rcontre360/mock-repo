986b0be10598efc69c9921dec2e1ef8f87ee3f45
Rcontre360 <rcontreraspimentel@gmail.com>
2021-03-14 21:22:57 -0400

    commit changes

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import git
 from git_contributions_importer import *
 
-repo = git.Repo("/home/rafael/Desktop/work/ongoing/ongoing")
+repo = git.Repo("/home/rafael/Desktop/work/bizorRepo/bizor-next")
 mock_repo = git.Repo("/home/rafael/Desktop/sandbox/gitRepos/contributionImporter")
 
 importer = Importer([repo], mock_repo)
